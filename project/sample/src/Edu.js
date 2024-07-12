7b0831911ace395867e5f8e3871bc0acbc5b2cb4
Gangadhar <ganga@gmail.com>
2024-07-12 15:43:40 +0530

    first commit

---
+++ project/sample/src/Edu.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Edu = () => {
+  return (
+    <div>
+        <h1>Education:-</h1>
+        <div>
+        <div className='dis'>
+            <div><h4>Annamachrya Institute of Technology and Sciences Rajampet</h4></div>
+            <div className='display'><h4>2021-25</h4></div>
+        </div>
+        <li>EEE</li>
+        </div>
+        <div>
+        <div className='dis'>
+            <div><h4>Sri Shirdi Sai Jr college Rajampet</h4></div>
+            <div className='display'><h4>2019-21</h4></div>
+        </div>
+        <li>MPC</li>
+        </div>
+        <div>
+        <div className='dis'>
+            <div><h4>Raju High School Mannur Rajampet</h4></div>
+            <div className='display'><h4>2018-19</h4></div>
+        </div>
+        <li>SSC</li>
+        </div>
+    </div>
+  )
+}
+
+export default Edu
---
+++ project/sample/src/Img.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Img = () => {
+  return (
+    <div className='img'>
+        <img className='image' src='c:\Users\venkatesh\Downloads\gangadhar.jpg'/>
+    </div>
+  )
+}
+
+export default Img
